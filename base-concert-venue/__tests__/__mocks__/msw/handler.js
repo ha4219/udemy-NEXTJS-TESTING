f5075bdd470ab0ffc991939b38d1f0556718724a
ha4219 <ha4219@naver.com>
2022-11-20 22:45:54 +0900

    #33 Different MSW Responses per Test

--- base-concert-venue/__tests__/__mocks__/msw/handler.js
+++ base-concert-venue/__tests__/__mocks__/msw/handler.js
@@ -8,7 +8,11 @@ const baseURL = "http://localhost:3000";
 export const handlers = [
   rest.get(`${baseURL}/api/shows/:showId`, async (req, res, ctx) => {
     const { fakeShows } = await readFakeData();
-    return res(ctx.json({ show: fakeShows[0] }));
+    const { showId } = req.params;
+
+    // index / showId = 0 has seats available in fake data
+    // index / showId = 1 has NO seats available
+    return res(ctx.json({ show: fakeShows[showId] }));
   }),
   rest.get(`${baseURL}/api/users/:userId/reservations`, (req, res, ctx) =>
     res(ctx.json({ userReservations: fakeUserReservations }))
