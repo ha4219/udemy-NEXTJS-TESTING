194ccd1fd2ee178e42193ad72bdaf0cb4e12a3c1
ha4219 <ha4219@naver.com>
2022-11-21 00:37:31 +0900

    #42 Create a Method to Reset the Test DB

---
+++ base-concert-venue/__tests__/__mocks__/db/utils/reset-db.ts
@@ -0,0 +1,23 @@
+import { readFakeData } from "@/__tests__/__mocks__/fakeData";
+import { filenames, writeJSONToFile } from "@/lib/db/db-utils";
+
+export const resetDB = async () => {
+  // faile safe against resetting production db!
+  const safeToReset = process.env.NODE_ENV === "test";
+  if (!safeToReset) {
+    // eslint-disable-next-line no-console
+    console.log("WARNING: database reset unavailable outside test enviroment!");
+    // eslint-disable-next-line no-useless-return
+    return;
+  }
+
+  const { fakeShows, fakeBands, fakeUsers, fakeReservations } =
+    await readFakeData();
+  // overwrite data in files
+  await Promise.all([
+    writeJSONToFile(filenames.bands, fakeBands),
+    writeJSONToFile(filenames.shows, fakeShows),
+    writeJSONToFile(filenames.reservations, fakeReservations),
+    writeJSONToFile(filenames.users, fakeUsers),
+  ]);
+};
