7d9ade7d29b2c5a13817b07935dedb95f25b4874
ha4219 <ha4219@naver.com>
2022-11-20 22:33:20 +0900

    #32 Code Quiz! User Reservations

--- base-concert-venue/__tests__/__mocks__/msw/handler.js
+++ base-concert-venue/__tests__/__mocks__/msw/handler.js
@@ -1,10 +1,16 @@
 import { rest } from "msw";
 
 import { readFakeData } from "../fakeData";
+import { fakeUserReservations } from "../fakeData/userReservations";
+
+const baseURL = "http://localhost:3000";
 
 export const handlers = [
-  rest.get("http://localhost:3000/api/shows/:showId", async (req, res, ctx) => {
+  rest.get(`${baseURL}/api/shows/:showId`, async (req, res, ctx) => {
     const { fakeShows } = await readFakeData();
-    return res(ctx.json({ shows: fakeShows[0] }));
+    return res(ctx.json({ show: fakeShows[0] }));
   }),
+  rest.get(`${baseURL}/api/users/:userId/reservations`, (req, res, ctx) =>
+    res(ctx.json({ userReservations: fakeUserReservations }))
+  ),
 ];
---
+++ base-concert-venue/__tests__/ui/user-reservations.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+
+import { UserReservations } from "@/components/user/UserReservations";
+
+test("test", async () => {
+  render(<UserReservations userId={1} />);
+
+  const name = await screen.findByText(/The Joyous Nun Riot/i);
+  expect(name).toBeInTheDocument();
+
+  const btn = await screen.findByRole("button", {
+    name: /Purchase more tickets/i,
+  });
+  expect(btn).toBeInTheDocument();
+});
