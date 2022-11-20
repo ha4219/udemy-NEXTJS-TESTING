0c6d27f3e3563ffd413db418e143d7cd5d06f65c
ha4219 <ha4219@naver.com>
2022-11-21 01:08:46 +0900

    #52 First Cypress Test: Static Route

---
+++ base-concert-venue/cypress/integration/routes.test.js
@@ -0,0 +1,9 @@
+it("display correct heading when navigating to shows route", () => {
+  cy.visit("/");
+  cy.findByRole("button", {
+    name: /shows/i,
+  }).click();
+  cy.findByRole("heading", {
+    name: /upcoming shows/i,
+  }).should("exist");
+});
