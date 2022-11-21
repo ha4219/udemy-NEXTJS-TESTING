5fcb0592d5122702188c8c49e67c79e5b34b134b
ha4219 <ha4219@naver.com>
2022-11-22 00:40:23 +0900

    #66 not working why?

--- base-concert-venue/cypress/integration/shows-isr.test.js
+++ base-concert-venue/cypress/integration/shows-isr.test.js
@@ -2,7 +2,7 @@ it("skips client-side bundle, confirming data from ISR cache", () => {
   cy.request("/shows")
     .its("body")
     .then((html) => {
-      const staticHtml = html.replace(/<script.*?>.*?<\/script.*?>/gm, "");
+      const staticHtml = html.replace(/<script.*?>.*?<\/script>/gm, "");
       cy.state("document").write(staticHtml);
     });
 
