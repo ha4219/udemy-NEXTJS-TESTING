64fc33dec2d0ed4eb51ef250fdaf3070aa5eb3bd
ha4219 <ha4219@naver.com>
2022-11-25 01:00:32 +0900

    #87 Code Quiz! Authenticating Programmatically

--- base-concert-venue/cypress/integration/auth/auth-wrapper.test.js
+++ base-concert-venue/cypress/integration/auth/auth-wrapper.test.js
@@ -106,3 +106,17 @@ it("does not show sign-in page when already signed in", () => {
 
   cy.findByRole("button", { name: /purchase/i }).should("exist");
 });
+
+it("displays Shows page after clicking 'purchase more tickets' button", () => {
+  cy.task("db:reset").signIn(
+    Cypress.env("TEST_USER_EMAIL"),
+    Cypress.env("TEST_PASSWORD")
+  );
+
+  cy.visit("/user");
+
+  cy.findByRole("button", { name: /purchase more tickets/i })
+    .should("exist")
+    .click();
+  cy.findByRole("heading", { name: /upcoming shows/i }).should("exist");
+});
