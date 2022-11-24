e7fb943add9e375329b92feaafba73960c19a05c
ha4219 <ha4219@naver.com>
2022-11-25 00:10:30 +0900

    #83 Code Quiz! Authentication Failure followed by Success

--- base-concert-venue/cypress/integration/auth-wrapper.test.js
+++ base-concert-venue/cypress/integration/auth-wrapper.test.js
@@ -32,3 +32,52 @@ it("runs auth flow for successful login to protected reservations page", () => {
 
   cy.findByRole("button", { name: /sign in/i }).should("not.exist");
 });
+
+it("runs auth flow for protected user page, including failed sign in", () => {
+  // visit reservations page for the first show (id = 0)
+  cy.task("db:reset").visit("/user");
+
+  cy.findByRole("heading", { name: /Sign in to your account/i }).should(
+    "exist"
+  );
+
+  cy.findByRole("button", { name: /purchase/i }).should("not.exist");
+
+  cy.findByLabelText(/email address/i)
+    .clear()
+    .type(Cypress.env("TEST_USER_EMAIL"));
+
+  cy.findByLabelText(/password/i)
+    .clear()
+    .type(`not my password`);
+
+  cy.findByRole("main").within(() => {
+    cy.findByRole("button", {
+      name: /sign in/i,
+    }).click();
+  });
+
+  cy.findByText(/sign in failed/i).should("exist");
+
+  cy.findByLabelText(/email address/i)
+    .clear()
+    .type(Cypress.env("TEST_USER_EMAIL"));
+
+  cy.findByLabelText(/password/i)
+    .clear()
+    .type(Cypress.env("TEST_PASSWORD"));
+
+  cy.findByRole("main").within(() => {
+    cy.findByRole("button", {
+      name: /sign in/i,
+    }).click();
+  });
+
+  cy.findByRole("heading", { name: /welcome/i }).should("exist");
+
+  cy.findByRole("heading", { name: /your tickets/i }).should("exist");
+
+  cy.findByRole("button", { name: /sign out/i }).should("exist");
+
+  cy.findByRole("button", { name: /sign in/i }).should("not.exist");
+});
