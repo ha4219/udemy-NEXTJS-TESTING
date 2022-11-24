69781999934981f632610632d0af97d62a1b4c25
ha4219 <ha4219@naver.com>
2022-11-24 20:24:07 +0900

    #76 Code Quiz! Revalidate on Interval

--- base-concert-venue/cypress/integration/swr-revalidate.test.js
+++ base-concert-venue/cypress/integration/swr-revalidate.test.js
@@ -2,28 +2,59 @@ import { generateNewReservation } from "../../__tests__/__mocks__/fakeData/newRe
 import { generateRandomId } from "../../lib/features/reservations/utils";
 
 const ONE_SECOND = 1000;
-const THIRTY_SECOND = 30 * ONE_SECOND;
+const FIFTEEN_SECONDS = 15 * ONE_SECOND;
+const THIRTY_SECONDS = 30 * ONE_SECOND;
 
-it("should refresh the show page after 30 seconds", () => {
-  cy.clock();
-  cy.task("db:reset").visit("/shows");
+describe("swr revalidate test", () => {
+  it("should refresh the show page after 30 seconds", () => {
+    cy.clock();
+    cy.task("db:reset").visit("/shows");
 
-  // there should be only one sold-out show
-  cy.findAllByText(/sold out/i).should("have.length", 1);
+    // there should be only one sold-out show
+    cy.findAllByText(/sold out/i).should("have.length", 1);
 
-  // buy all tickets for first show (id 0, 10 seats availabe)
-  const newReservation = generateNewReservation({
-    reservationId: generateRandomId(),
-    showId: 0,
-    seatCount: 10,
+    // buy all tickets for first show (id 0, 10 seats availabe)
+    const newReservation = generateNewReservation({
+      reservationId: generateRandomId(),
+      showId: 0,
+      seatCount: 10,
+    });
+    cy.task("addReservation", newReservation);
+
+    // advance time (less than 30 second revalidate interval) and check again
+    cy.tick(ONE_SECOND);
+    cy.findAllByText(/sold out/i).should("have.length", 1);
+
+    // advance clock 30 seconds more; now sold out show should display
+    cy.tick(THIRTY_SECONDS);
+    cy.findAllByText(/sold out/i).should("have.length", 2);
   });
-  cy.task("addReservation", newReservation);
 
-  // advance time (less than 30 second revalidate interval) and check again
-  cy.tick(ONE_SECOND);
-  cy.findAllByText(/sold out/i).should("have.length", 1);
+  it("should refresh the reservations page after fifteen seconds", () => {
+    cy.clock();
+    cy.task("db:reset").visit("/reservations/0");
 
-  // advance clock 30 seconds more; now sold out show should display
-  cy.tick(THIRTY_SECOND);
-  cy.findAllByText(/sold out/i).should("have.length", 2);
+    cy.findByRole("main").within(() =>
+      cy.findByRole("button", { name: /sign in/i }).click()
+    );
+
+    // there should be only one sold-out show
+    cy.findByText(/10 seats left/i).should("exist");
+
+    // buy all tickets for first show (id 0, 10 seats availabe)
+    const newReservation = generateNewReservation({
+      reservationId: 12345,
+      showId: 0,
+      seatCount: 2,
+    });
+    cy.task("addReservation", newReservation);
+
+    // advance time (less than 30 second revalidate interval) and check again
+    cy.tick(ONE_SECOND);
+    cy.findByText(/10 seats left/i).should("exist");
+
+    // advance clock 30 seconds more; now sold out show should display
+    cy.tick(THIRTY_SECONDS);
+    cy.findByText(/8 seats left/i).should("exist");
+  });
 });
